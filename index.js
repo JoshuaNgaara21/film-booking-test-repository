f26d6363fad99dfe0eff561bb37398902b9059eb
+ <joshngaara@gmail.com>
2025-01-09 04:22:14 +0200

    have acquired json file data but not displaying in html

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-fetch('FlatDango Db Data  (1).json')
-.then(res => res.json())
+let listEl = document.querySelector('ul');
+ let arr = [], data;
+fetch('./FlatDango Db Data  (1).json')
+.then(response => response.json())
 .then(data => {
-  console.log(data);
-  
+    console.log(data);
+    const films= data; // creating another data var to try iterate outside fetch
+    document.querySelector('p').insertAdjacentElement = films.title;
+    
+    // for (const element of data) {
+    // arr
+        
+    // }
+    Object.keys(data).forEach(key => {
+    const value = data[key];
+    console.log(`${key}: ${value}`)
+});
 })
